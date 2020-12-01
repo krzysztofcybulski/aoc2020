cf5d6941206b1b115bce0c6b59958f3a05e4e030
Krzysztof Cybulski <krzysztof.cybulski@allegro.pl>
2020-12-01 10:03:20 +0100

    Day 1: Report Repair - part 1

--- src/01ReportRepair.kt
+++ src/01ReportRepair.kt
@@ -2,13 +2,13 @@ class ReportRepair(expenses: Collection<Int>) {
 
     val expenses = HashSet(expenses)
 
-    fun result(index: Int = 0): Int = expenses
-        .find { expenses.contains(2020 - it) }
-        ?.let { it * (2020 - it) }
+    fun productOf2EntriesThatSumTo(sum: Int): Int = expenses
+        .find { expenses.contains(sum - it) }
+        ?.let { it * (sum - it) }
         ?: throw IllegalStateException("No such expenses")
 
 }
 
 fun main(args: Array<String>) {
-    println(ReportRepair(args.map { it.toInt() }).result())
+    println(ReportRepair(args.map { it.toInt() }).productOf2EntriesThatSumTo(2020))
 }
