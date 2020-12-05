755070b3a975c98bae58802cbdae933e35c39cca
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2020-12-05 12:21:10 +0100

    Day 5: Binary Boarding - part 2

---
+++ src/05BinaryBoarding.kt
@@ -0,0 +1,37 @@
+class Plane(private val takenSeats: Collection<Seat>) {
+
+    fun findEmptySeats() = (takenSeats.minOf { it.id }..takenSeats.maxOf { it.id }) - takenSeats.map { it.id }
+
+}
+
+class Seat(row: Int, column: Int) {
+
+    val id = row * 8 + column
+
+    companion object {
+
+        fun from(description: String): Seat {
+            val row = binary(description.substring(0, 7).map { it == 'B' }, 128)
+            val column = binary(description.substring(7).map { it == 'R' }, 8)
+            return Seat(row, column)
+        }
+
+        private fun binary(rights: List<Boolean>, max: Int, min: Int = 0): Int = when (rights.firstOrNull()) {
+            true -> binary(rights.drop(1), max = max, min = (max - min) / 2 + min)
+            false -> binary(rights.drop(1), max = (max - min) / 2 + min, min = min)
+            null -> min
+        }
+
+    }
+
+}
+
+fun main() {
+    val lines = object {}.javaClass.getResource("05BinaryBoarding.txt")
+        .readText()
+        .split("\n")
+        .filter { it.isNotBlank() }
+
+    val plane = Plane(lines.map { Seat.from(it) })
+    println(plane.findEmptySeats())
+}
