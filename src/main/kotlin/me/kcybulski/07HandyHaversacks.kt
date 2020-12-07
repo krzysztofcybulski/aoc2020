8ec89badc69d8a8ecaed8aec3556f6b753c32d5b
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2020-12-07 06:48:02 +0100

    Day 7: Handy Haversacks - part 2

--- src/main/kotlin/me/kcybulski/07HandyHaversacks.kt
+++ src/main/kotlin/me/kcybulski/07HandyHaversacks.kt
@@ -1,34 +1,33 @@
 package me.kcybulski
 
-data class Bag(val name: String, val innerBags: Collection<Bag>) {
+data class Bag(val name: String, val innerBags: Collection<Pair<Int, Bag>>) {
 
-    fun contains(color: String): Boolean = when(name) {
-        color -> true
-        else -> innerBags.any { it.contains(color) }
-    }
+//    fun contains(color: String): Boolean = when(name) {
+//        color -> true
+//        else -> innerBags.any { (_, bag) -> bag.contains(color) }
+//    }
+
+    fun size(): Int = 1 + (innerBags.sumBy { (amount, bag) -> amount * bag.size() })
 
 }
 
 fun main() {
-    val bagsMap: Map<String, List<String>> = lines("07HandyHaversacks")
+    val bagsMap: Map<String, List<Pair<Int, String>>> = lines("07HandyHaversacks")
         .map { parse(it) }
         .associateBy({ it.first }, { it.second })
     val bags = bagsMap.keys.map { createBag(it, bagsMap) }
-    println(bags.count { it.contains("shiny gold") })
+    println(bags.find { it.name == "shiny gold" }?.size())
 }
 
-private fun createBag(name: String, bags: Map<String, List<String>>): Bag =
-    Bag(name, (bags[name] ?: emptyList()).map { createBag(it, bags) })
+private fun createBag(name: String, bags: Map<String, List<Pair<Int, String>>>): Bag =
+    Bag(name, (bags[name] ?: emptyList<Pair<Int, String>>()).map { Pair(it.first, createBag(it.second, bags)) })
 
 private val bagsRegex = "([1-9][0-9]*) ([a-z]+ [a-z]+) bags?[,|.]\\s*".toRegex()
 
-private fun parse(line: String): Pair<String, List<String>> {
+private fun parse(line: String): Pair<String, List<Pair<Int, String>>> {
     val split = line.split(" bags contain ")
     val bags = bagsRegex.findAll(split[1])
-        .map { Pair(it.groupValues[1], it.groupValues[2]) }
-        .map { (amount, name) -> name }
+        .map { Pair(it.groupValues[1].toInt(), it.groupValues[2]) }
         .toList()
     return Pair(split[0], bags)
 }
-
-private fun bags(amount: Int, name: String) = (1..amount).map { name }
