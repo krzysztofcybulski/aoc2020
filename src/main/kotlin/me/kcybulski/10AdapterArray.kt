d0fe172b741ecc8c642bbc4564a5255c04dee132
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2020-12-10 17:31:29 +0100

    Day 10: Adapter Array - part 2

--- src/main/kotlin/me/kcybulski/10AdapterArray.kt
+++ src/main/kotlin/me/kcybulski/10AdapterArray.kt
@@ -9,14 +9,23 @@ fun main(args: Array<String>) {
         .map { it.toInt() }
         .sorted()
 
-    val diffs = differences(lines, 0)
-    println((diffs.count { it.jolts == 3 } + 1)* diffs.count { it.jolts == 1 })
+    val adapters = listOf(0) + lines + (lines.maxOrNull() ?: 0 + 3)
+
+    val diffs = differences(adapters, 0)
+    println(diffs.count { it.jolts == 3 } * diffs.count { it.jolts == 1 })
+    println(ways(adapters))
 }
 
 fun differences(input: List<Int>, compare: Int, output: List<Difference> = emptyList()): List<Difference> =
-    if(input.isEmpty()) {
+    if (input.isEmpty()) {
         output
     } else {
         val first = input.first()
         differences(input.drop(1), first, output + Difference(first - compare))
     }
+
+fun ways(input: List<Int>): Long =
+    input.drop(1)
+        .fold(mapOf(0 to 1L)) { acc, adapter ->
+            acc + (adapter to ((adapter - 3) until adapter).map { acc[it] ?: 0L }.sum())
+        }[input.last()] ?: 0L
