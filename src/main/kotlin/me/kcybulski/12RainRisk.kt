3c8ae9609e800b68515f2eace5812d2c66a2c481
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2020-12-12 06:57:03 +0100

    Day 12 | Rain Risk - day 2

--- src/main/kotlin/me/kcybulski/12RainRisk.kt
+++ src/main/kotlin/me/kcybulski/12RainRisk.kt
@@ -1,13 +1,33 @@
 package me.kcybulski
 
 import me.kcybulski.utils.lines
-import java.lang.RuntimeException
+import kotlin.math.PI
 import kotlin.math.absoluteValue
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class RainRisk() {
 }
 
-data class FerryPosition(val direction: Direction, val north: Int, val east: Int) {
+data class WayPoint(val degrees: Double, val distance: Int) {
+
+    fun north() = (distance * sin(degrees)).roundToInt()
+    fun east() = (distance * cos(degrees)).roundToInt()
+
+    companion object {
+
+        fun from(x: Int, y: Int) = WayPoint(
+            atan2(y.toDouble(), x.toDouble()),
+            sqrt(x.toDouble().pow(2.0) + y.toDouble().pow(2.0)).roundToInt()
+        )
+    }
+}
+
+data class FerryPosition(val north: Int, val east: Int, val wayPoint: WayPoint) {
 
     fun result() = north.absoluteValue + east.absoluteValue
 }
@@ -19,24 +39,28 @@ sealed class Command {
 }
 
 class Move(private val direction: Direction, private val move: Int) : Command() {
-    override fun next(position: FerryPosition): FerryPosition = when (direction) {
-        Direction.NORTH -> position.copy(north = position.north + move)
-        Direction.EAST -> position.copy(east = position.east + move)
-        Direction.SOUTH -> position.copy(north = position.north - move)
-        Direction.WEST -> position.copy(east = position.east - move)
-    }
+    override fun next(position: FerryPosition): FerryPosition = position.copy(
+        wayPoint = when (direction) {
+            Direction.NORTH -> WayPoint.from(y = position.wayPoint.north() + move, x = position.wayPoint.east())
+            Direction.EAST -> WayPoint.from(y = position.wayPoint.north(), x = position.wayPoint.east() + move)
+            Direction.SOUTH -> WayPoint.from(y = position.wayPoint.north() - move, x = position.wayPoint.east())
+            Direction.WEST -> WayPoint.from(y = position.wayPoint.north(), x = position.wayPoint.east() - move)
+        }
+    )
 }
 
 class Rotate(private val rotation: Rotation, private val degrees: Int) : Command() {
-    override fun next(position: FerryPosition): FerryPosition {
-        val rotateTimes = degrees / 90
-        val dirs = if (rotation == Rotation.RIGHT) Direction.values() else Direction.values().reversedArray()
-        return position.copy(direction = dirs[(rotateTimes + dirs.indexOf(position.direction)) % 4])
-    }
+    override fun next(position: FerryPosition): FerryPosition = position.copy(
+        wayPoint = position.wayPoint.copy(
+            position.wayPoint.degrees + ((if (rotation == Rotation.RIGHT) -degrees else degrees) * PI / 180)
+        )
+    )
 }
 
-class Forward(private val move: Int): Command() {
-    override fun next(position: FerryPosition): FerryPosition = Move(position.direction, move).next(position)
+class Forward(private val move: Int) : Command() {
+    override fun next(position: FerryPosition): FerryPosition {
+        return position.copy(north = position.north + (position.wayPoint.north() * move), east = position.east + (position.wayPoint.east() * move))
+    }
 }
 
 enum class Direction {
@@ -66,7 +90,8 @@ fun main() {
             }
         }
 
-    val startPosition = FerryPosition(Direction.EAST, 0, 0)
+    val wayPoint = WayPoint.from(10, 1)
+    val startPosition = FerryPosition(0, 0, wayPoint)
     val endPosition = lines.fold(startPosition) { acc, pos -> pos.next(acc) }
 
     println(endPosition.result())
